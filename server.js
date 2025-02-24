835585af2f01076eefa892f8fcf46b5d0fbd70bf
Michael Neeley <micneeley14@gmail.com>
2025-02-23 19:52:34 -0500

    port

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
